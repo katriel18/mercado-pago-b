c5a269acfd98732586bce006d33fa76392e7204d
Osti Katriel <katriel181920@gmail.com>
2021-02-21 22:22:48 -0500

    API rest pay item

---
+++ src/main/java/osti/katriel/demomercadopagob/DemoMercadoPagoBApplication.java
@@ -0,0 +1,13 @@
+package osti.katriel.demomercadopagob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMercadoPagoBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMercadoPagoBApplication.class, args);
+	}
+
+}
